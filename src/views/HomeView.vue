af7875afc70e4e2bab02b1994f240d3944e5504e
kiwixin <103304325@qq.com>
2023-05-24 17:18:56 +0800

    5241718： 宫格完善，背景完善

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,52 +1,213 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div>
     <div> <!--标题-->
       <h1>CAP的日用品</h1>
     </div>
     <!-- 搜索（后期功能，占位） -->
-    <div></div>
+    <div>
+      <input type="text" placeholder="搜索（不可用）" />
+    </div>
     <div> <!--详情-->
       <!-- 卡片外围 -->
       <div class="grid-container">
         <!-- 卡片内容开始 宫格样式 -->
-        <div class="card">
+        <!-- 523:改用新选项卡打开网页 -->
+        <div class="card" @click="OpenLink('https://www.xiaomi.cn/')">
           <!-- 左头像 -->
           <div class="card-header">
             <!-- 图片 -->
-            <img class="avatar" src="../assets/OIP-C.jpg" alt="小米网"/>
+            <img class="avatar" src="../assets/OIP-C.jpg" alt="小米社区"/>
           </div>
           <!-- 右边内容 -->
           <div class="card-content">
             <!-- 内容 -->
             <!-- 右边标题 -->
             <div class="title">
-              <h2>小米网</h2>
+              <h2>小米社区</h2>
             </div>
             <div class="content">
-            Hello，我是小米网
+            前往小米社区
             </div>
           </div>
         </div>
         <!-- 卡片结束/外围 -->
         <!-- 卡片内容开始 宫格样式 -->
-        <div class="card">
+        <div class="card" @click="OpenLink('https://svpn.tjnu.edu.cn/')">
           <!-- 左头像 -->
           <div class="card-header">
             <!-- 图片 -->
-            <img class="avatar" src="../assets/OIP-C.jpg" alt="小米网"/>
+            <img class="avatar" src="../assets/logo.png" alt="师大svpn"/>
           </div>
           <!-- 右边内容 -->
           <div class="card-content">
             <!-- 内容 -->
             <!-- 右边标题 -->
             <div class="title">
-              <h2>小米网</h2>
+              <h2>天津师范大学SVPN</h2>
             </div>
             <div class="content">
-            Hello，我是小米网
+            可直接登录或下载客服端
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <div class="card" @click="OpenLink('https://mail.tjnu.edu.cn/coremail/')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/tnu_mail.png" alt="师大mail"/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>天津师范邮箱系统</h2>
+            </div>
+            <div class="content">
+            顾名思义，去查邮件的
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <div class="card" @click="OpenLink('https://jwxt.tjnu.edu.cn/tjsfjw/cas/login.action')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/logo.png" alt="师大教务"/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>天津师范大学教务系统</h2>
+            </div>
+            <div class="content">
+            卡死了。。垃圾系统
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <!-- 523:改用新选项卡打开网页 -->
+        <div class="card" @click="OpenLink('https://roms.miuier.com/zh-cn/')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/miuidl_favicon.png" alt="小米miui下载 "/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>小米MIUI下载</h2>
+            </div>
+            <div class="content">
+            miui全机型下载
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <div class="card" @click="OpenLink('https://oneplus.com')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/oneplus.jpg" alt="一加"/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>一加官网</h2>
+            </div>
+            <div class="content">
+            下载ROM
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <div class="card" @click="OpenLink('https://jwxt.tjnu.edu.cn/tjsfjw/cas/login.action')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/cgpt_favicon.png" alt="chatgpt"/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>ChatGPT</h2>
+            </div>
+            <div class="content">
+            8说了，yyds
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <div class="card" @click="OpenLink('https://chatbot.theb.ai/')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/baichat_favicon.png" alt="baichat"/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>BAIChat</h2>
+            </div>
+            <div class="content">
+            备用，也很8错
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <div class="card" @click="OpenLink('https://worldvectorlogo.com/')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/getlogo_favicon.png" alt="获取logo"/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>WORLDVECTORLOGO</h2>
+            </div>
+            <div class="content">
+            获取logo的，也很好用
+            </div>
+          </div>
+        </div>
+        <!-- 卡片结束/外围 -->
+        <!-- 卡片内容开始 宫格样式 -->
+        <div class="card" @click="OpenLink('https://worldvectorlogo.com/')">
+          <!-- 左头像 -->
+          <div class="card-header">
+            <!-- 图片 -->
+            <img class="avatar" src="../assets/getlogo_favicon.png" alt="获取logo"/>
+          </div>
+          <!-- 右边内容 -->
+          <div class="card-content">
+            <!-- 内容 -->
+            <!-- 右边标题 -->
+            <div class="title">
+              <h2>01234567890</h2>
+            </div>
+            <div class="content">
+            测试字体ABCDEJRQWERTYUIOPASDFGHJKLZXCVBNM
             </div>
           </div>
         </div>
@@ -57,7 +218,31 @@
     </div>
   </div>
 </template>
+<script setup lang="ts">
+
+  const OpenLink=(link:string)=>{
+    window.open(link,"_blank")
+  }
+  
+
+</script>
 <style>
+  @font-face {
+    font-family: custom;
+    src: 
+         url(@/assets/OPPOSans3.0cn-Regular.ttf) format('truetype');
+  }
+  body{
+    background: url(@/assets/cap_bg.svg) center center/cover no-repeat;
+    font-family: 'custom';
+    
+  }
+  a{
+    text-decoration: none;
+  }
+  a:visited{
+    color: inherit;
+  }
   .grid-container {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
@@ -79,7 +264,6 @@
     width: 50px;
     height: 50px;
     border-radius: 50%;
-    margin-right: 10px;
   }
   .card-info {
     display: flex;
